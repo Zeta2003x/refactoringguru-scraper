da5f6a1f559e5b29526ef5591e5ee8e0ef414387
Matias Grin <mgrin@frba.utn.edu.ar>
2023-08-13 01:14:24 -0300

    Agrego logica

--- index.py
+++ index.py
@@ -3,17 +3,57 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.window import WindowTypes
 
 
+def abrir_catalogo():
+    catalogo_url = "https://refactoring.guru/es/design-patterns/catalog"
+    driver.get(catalogo_url)
 
-def trabajar_patron(url_patron:str):
+
+def abrir_patron(url_patron:str):
     driver.switch_to.new_window(WindowTypes.TAB)
     driver.get(url_patron)
-    print(driver.find_element(By.XPATH, "/html/body/div/main/div/div/article/div[3]/p").text)
+
+
+def obtener_informacion():
+    # Proposito
+    proposito = driver.find_element(By.CSS_SELECTOR, ".intent > p").text
+    # Aplicabilidad
+    usos = driver.find_elements(By.CSS_SELECTOR, ".applicability-problem > p")
+    aplicabilidad = list(map((lambda uso: uso.text), usos))
+    return aplicabilidad, proposito
+
+
+def iterar_info(pat, args):
+    for arg in args:
+        if isinstance(arg, list): agregar_patron(arg)
+        else: pat.writelines(f"{arg}\n")
+
+
+def agregar_patron(*args):
+    if len(args) == 1: args = args[0]
+    with open("patrones.txt", "a+", encoding='utf8') as pat:
+        iterar_info(pat, args)
+
+
+def cerrar_patron():
     driver.close()
     driver.switch_to.window(driver.window_handles[0])
 
 
-driver = chrome_driver_setup.create_chrome_driver()
-original_window = "https://refactoring.guru/es/design-patterns/catalog"
-driver.get(original_window)
-patterns = driver.find_elements(By.CSS_SELECTOR, ".pattern-card")
-list(map((lambda pattern: trabajar_patron(pattern.get_attribute('href'))), patterns))
+def trabajar_patron(url_patron:str):
+    abrir_patron(url_patron)
+    informacion = obtener_informacion()
+    agregar_patron(*informacion)
+    with open("patrones.txt", "a+", encoding="utf-8") as pat:
+        pat.writelines("\n")
+    cerrar_patron()
+
+
+def main():
+    abrir_catalogo()
+    patterns = driver.find_elements(By.CSS_SELECTOR, ".pattern-card")
+    list(map((lambda pattern: trabajar_patron(pattern.get_attribute('href'))), patterns))
+
+
+if __name__ == '__main__':
+    driver = chrome_driver_setup.create_chrome_driver()
+    main()
